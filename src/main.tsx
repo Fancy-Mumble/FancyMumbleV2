fb9955294ef0384aa53dfb6a0db6552ab264964b
Sebastian <19Sebastian95@gmx.de>
2023-04-30 17:12:32 +0200

    GH-16: Introduce state management for users

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material';
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 const darkTheme = createTheme({
   palette: {
@@ -13,7 +15,9 @@ const darkTheme = createTheme({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
+    <Provider store={store}>
     <App />
+    </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 )
