b4def5eea5eeea0dc9b76f0fcb228492b67c41c0
Sebastian <19Sebastian95@gmx.de>
2023-05-10 21:51:16 +0200

    Added code to enable user mute

--- src/components/CurrentUserInfo.tsx
+++ src/components/CurrentUserInfo.tsx
@@ -3,9 +3,21 @@ import { RootState } from "../store/store";
 import { getBackgroundFromComment, getProfileImage } from "../helper/UserInfoHelper";
 import { Avatar, Badge, Box, Container } from "@mui/material";
 import ChannelSearch from "./ChannelSearch";
+import MicIcon from '@mui/icons-material/Mic';
+import './styles/CurrentUserInfo.css'
+import { invoke } from "@tauri-apps/api";
 
 function CurrentUserInfo() {
     const userInfo = useSelector((state: RootState) => state.reducer.userInfo);
+
+    function muteToggleUser() {
+        if (userInfo.currentUser) {
+            //TODO: This won't work, because we don't have a setter for the currentUser (mute) property
+            userInfo.currentUser.mute = !userInfo.currentUser.mute;
+            invoke('change_user_state', { userId: userInfo.currentUser.id, userInfo: userInfo.currentUser });
+        }
+    }
+
     return (
         <Box style={{
             background: getBackgroundFromComment(userInfo.currentUser),
@@ -26,7 +38,10 @@ function CurrentUserInfo() {
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '2px 10px', flexDirection: 'column', alignItems: 'center', width: '100%', textShadow: '1px 1px #000' }}>
-                    {userInfo.currentUser?.name ?? 'Unknown'}
+                    <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        {userInfo.currentUser?.name ?? 'Unknown'}
+                        <MicIcon onClick={() => muteToggleUser()} className="small_icon" />
+                    </Box>
                 </Box>
                 <ChannelSearch />
             </Box>
