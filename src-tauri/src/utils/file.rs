0b0cab0eed400c8d72a39508999f534d80a65fc9
Sebastian <19Sebastian95@gmx.de>
2023-05-18 21:15:48 +0200

    GH-44: Fix profile and background images

--- src-tauri/src/utils/file.rs
+++ src-tauri/src/utils/file.rs
@@ -1,12 +1,27 @@
+use std::fmt::Display;
+
+use image::imageops::FilterType;
+use image::GenericImageView;
 use tokio::fs::{self, File};
-use tokio::io::AsyncReadExt;
+use tokio::io::{AsyncReadExt, BufReader};
+use tracing::debug;
 
 use crate::errors::AnyError;
 
+pub struct ImageInfo {
+    pub data: Vec<u8>,
+    pub format: ImageFormat,
+}
+
+pub struct ImageFormat {
+    pub format: image::ImageFormat,
+}
+
 // we check that the file is not too large
 #[allow(clippy::cast_possible_truncation)]
+#[allow(unused)]
 pub async fn get_file_as_byte_vec(filename: &str) -> AnyError<Vec<u8>> {
-    let mut f = File::open(&filename).await?;
+    let mut f = BufReader::new(File::open(&filename).await?);
     let metadata = fs::metadata(&filename).await?;
     let mut buffer = vec![0; metadata.len() as usize];
     if buffer.len() > u32::MAX as usize {
@@ -16,9 +31,10 @@ pub async fn get_file_as_byte_vec(filename: &str) -> AnyError<Vec<u8>> {
         )));
     }
 
-    match f.read(&mut buffer).await {
+    match f.read_to_end(&mut buffer).await {
         Ok(read) => {
             if read == metadata.len() as usize {
+                debug!("Read {} bytes from {}", read, filename);
                 Ok(buffer)
             } else {
                 Err(Box::new(std::io::Error::new(
@@ -28,5 +44,56 @@ pub async fn get_file_as_byte_vec(filename: &str) -> AnyError<Vec<u8>> {
             }
         }
         Err(e) => Err(Box::new(e)),
+    }
+}
+
+pub fn read_image_as_thumbnail(filename: &str, max_size: u32) -> AnyError<ImageInfo> {
+    let mut output = Vec::new();
+    let buf_writer = std::io::BufWriter::new(&mut output);
+    let image = image::open(filename)?;
+
+    let image = image.resize(max_size, max_size, FilterType::Lanczos3);
+    let (width, height) = image.dimensions();
+    let color_type = image.color();
+
+    let image = image.into_bytes();
+
+    image::codecs::jpeg::JpegEncoder::new_with_quality(buf_writer, 80)
+        .encode(&image, width, height, color_type)?;
+
+    Ok(ImageInfo {
+        data: output,
+        format: ImageFormat {
+            format: image::ImageFormat::Jpeg,
+        },
+    })
+}
+
+impl Display for ImageFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.format {
+            image::ImageFormat::Png => write!(f, "png"),
+            image::ImageFormat::Jpeg => write!(f, "jpeg"),
+            image::ImageFormat::Gif => write!(f, "gif"),
+            image::ImageFormat::WebP => write!(f, "webp"),
+            image::ImageFormat::Pnm => write!(f, "pnm"),
+            image::ImageFormat::Tiff => write!(f, "tiff"),
+            image::ImageFormat::Tga => write!(f, "tga"),
+            image::ImageFormat::Dds => write!(f, "dds"),
+            image::ImageFormat::Bmp => write!(f, "bmp"),
+            image::ImageFormat::Ico => write!(f, "ico"),
+            image::ImageFormat::Hdr => write!(f, "hdr"),
+            image::ImageFormat::Farbfeld => write!(f, "farbfeld"),
+            image::ImageFormat::Avif => write!(f, "avif"),
+            image::ImageFormat::OpenExr => write!(f, "exr"),
+            image::ImageFormat::Qoi => write!(f, "qoi"),
+            _ => todo!(),
+        }
+    }
+}
+
+impl From<image::ImageFormat> for ImageFormat {
+    fn from(format: image::ImageFormat) -> Self {
+        Self { format }
     }
 }
