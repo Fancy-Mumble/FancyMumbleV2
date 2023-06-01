4985df8d1a71b91692ce8ed2ed4828cc63929cef
Sebastian <info@magical.rocks>
2023-06-01 22:11:37 +0200

    GH-2: Add Link preview for some websites

--- src/helper/MessageParser.tsx
+++ src/helper/MessageParser.tsx
@@ -1,10 +1,26 @@
 import { Box } from "@mui/material";
 import DOMPurify from "dompurify";
 import { marked } from "marked";
+import { createEmbeddedIFrame } from "./DOMHelper";
 
+interface LinkReplacement {
+    regex: RegExp;
+    replacement: string;
+    inline: boolean;
+}
 
 class DOMMessageParser {
     private document: Document;
+    private replacementUrl: LinkReplacement[] = [
+        { regex: /https:\/\/store\.steampowered\.com\/app\/([0-9]+)\/?.*/, replacement: 'steam://advertise/$1', inline: false },
+        { regex: /https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/, replacement: 'https://www.youtube.com/embed/$1', inline: true },
+        { regex: /https:\/\/www\.twitch\.tv\/videos\/([0-9]+)/, replacement: 'https://player.twitch.tv/?video=$1&parent='+(window.location.hostname), inline: true },
+        { regex: /https:\/\/clips\.twitch\.tv\/([a-zA-Z0-9_-]+)/, replacement: 'https://clips.twitch.tv/embed?clip=$1&parent='+(window.location.hostname), inline: true },
+        { regex: /https:\/\/www\.twitch\.tv\/([a-zA-Z0-9_-]+)/, replacement: 'https://player.twitch.tv/?channel=$1&parent='+(window.location.hostname), inline: true },
+        { regex: /https:\/\/twitter\.com\/([a-zA-Z0-9_]+)\/status\/([0-9]+)/, replacement: 'https://twitframe.com/show?url=https://twitter.com/$1/status/$2', inline: true },
+        { regex: /https:\/\/twitter\.com\/([a-zA-Z0-9_]+)/, replacement: 'https://twitframe.com/show?url=https://twitter.com/$1', inline: true },
+        { regex: /https:\/\/giphy.com\/gifs\/.*-([a-zA-Z0-9_-]+)/, replacement: 'https://giphy.com/embed/$1', inline: true },
+    ];
 
     constructor(input: string) {
         const parser = new DOMParser();
@@ -21,7 +37,21 @@ class DOMMessageParser {
 
     parseForLinks() {
         Array.from(this.document.querySelectorAll('a')).forEach(e => {
-            e.setAttribute('target', '_blank');
+            let replaced = !this.replacementUrl.every(r => {
+                if (e.getAttribute('href')?.match(r.regex)) {
+                    let replaced = e.getAttribute('href')?.replace(r.regex, r.replacement) || '';
+
+                    if (!r.inline) {
+                        e.setAttribute('href', replaced);
+                    } else {
+                        const b = createEmbeddedIFrame(replaced);
+                        e.parentElement?.replaceChild(b, e);
+                    }
+                    return false;
+                }
+                return true;
+            });
+            if (!replaced) e.setAttribute('target', '_blank');
         });
 
         return this;
